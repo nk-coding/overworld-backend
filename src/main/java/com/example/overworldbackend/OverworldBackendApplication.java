a6620932483234bca8b70244f3c21fb0c75d30c8
Levi <otterbach.levi@gmail.com>
2022-07-08 09:48:19 +0200

    init

---
+++ src/main/java/com/example/overworldbackend/OverworldBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.overworldbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OverworldBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OverworldBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/overworldbackend/OverworldBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.overworldbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OverworldBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
