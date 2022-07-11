9e8e489baae84126b73564782b0750a5036fbbd6
WuestFlorian <102306228+WuestFlorian@users.noreply.github.com>
2022-07-11 22:44:16 +0200

    Remove code smells
    remove code smells

--- src/main/java/com/example/overworldbackend/repositories/OverworldRepository.java
+++ src/main/java/com/example/overworldbackend/repositories/OverworldRepository.java
@@ -4,8 +4,6 @@ import com.example.overworldbackend.baseClasses.Configuration;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface OverworldRepository extends JpaRepository<Configuration, Long> {
     Configuration findByStaticWorldId(String staticWorldId);
