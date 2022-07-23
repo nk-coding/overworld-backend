b6c9d4d6e5f47d224c135fbb1c4977e7e81f679e
AaronSchimd <st166450@stud.uni-stuttgart.de>
2022-07-23 12:34:11 +0200

    fix tests

--- src/main/java/de/unistuttgart/overworldbackend/data/LectureInitialData.java
+++ src/main/java/de/unistuttgart/overworldbackend/data/LectureInitialData.java
@@ -4,10 +4,12 @@ import javax.validation.constraints.NotNull;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.experimental.FieldDefaults;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class LectureInitialData {
 
