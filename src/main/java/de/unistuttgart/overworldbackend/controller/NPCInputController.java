07f4315fa6697fd2220aae69bfcc852b11b70de0
Max Kästner <st172096@stud.uni-stuttgart.de>
2022-07-26 13:58:31 +0200

    change swagger description

--- src/main/java/de/unistuttgart/overworldbackend/controller/NPCInputController.java
+++ src/main/java/de/unistuttgart/overworldbackend/controller/NPCInputController.java
@@ -10,7 +10,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-@Tag(name = "Ignore this", description = "Ignore this here in swagger documentation :/")
+@Tag(name = "Submit game data", description = "Submit a NPC statistic")
 @RestController
 @Slf4j
 @RequestMapping("/internal")
