f3c39e16bd823a41c74e14de95117475c73bf1b4
Max Kästner <st172096@stud.uni-stuttgart.de>
2022-07-27 23:05:29 +0200

    remove unused dummy world

--- src/main/java/de/unistuttgart/overworldbackend/data/PlayerTaskStatistic.java
+++ src/main/java/de/unistuttgart/overworldbackend/data/PlayerTaskStatistic.java
@@ -7,7 +7,6 @@ import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
-
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Data;
