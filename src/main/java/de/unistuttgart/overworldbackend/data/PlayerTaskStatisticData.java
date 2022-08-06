24040a81662d18753c5fc9d984f397561fab57c9
AaronSchimd <st166450@stud.uni-stuttgart.de>
2022-08-06 13:36:27 +0200

    prettier

--- src/main/java/de/unistuttgart/overworldbackend/data/PlayerTaskStatisticData.java
+++ src/main/java/de/unistuttgart/overworldbackend/data/PlayerTaskStatisticData.java
@@ -5,7 +5,6 @@ import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.PositiveOrZero;
-
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Data;
