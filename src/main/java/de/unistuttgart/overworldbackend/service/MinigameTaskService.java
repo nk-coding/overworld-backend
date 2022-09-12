44d04e000ced807b3ef9cb7b076c31b3ee7a14a9
WuestFlorian <102306228+WuestFlorian@users.noreply.github.com>
2022-09-12 10:54:56 +0200

    Add missing import

--- src/main/java/de/unistuttgart/overworldbackend/service/MinigameTaskService.java
+++ src/main/java/de/unistuttgart/overworldbackend/service/MinigameTaskService.java
@@ -10,6 +10,7 @@ import de.unistuttgart.overworldbackend.data.mapper.MinigameTaskMapper;
 import de.unistuttgart.overworldbackend.repositories.MinigameTaskRepository;
 import java.util.Optional;
 import java.util.Set;
+import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
