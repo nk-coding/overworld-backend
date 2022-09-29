30654116b1a04c1af7749104f5fc32febce8607a
Aaron Schmid <56757286+Aaron2000119@users.noreply.github.com>
2022-09-29 17:27:27 +0200

    make constructor of roles private
    Co-authored-by: delvh <dev.lh@web.de>

--- src/main/java/de/unistuttgart/overworldbackend/data/Roles.java
+++ src/main/java/de/unistuttgart/overworldbackend/data/Roles.java
@@ -2,6 +2,8 @@ package de.unistuttgart.overworldbackend.data;
 
 import java.util.List;
 
-public class Roles {
+public final class Roles {
     public static final List<String> LECTURER_ROLE = List.of("lecturer");
+    
+    private Roles() {}
 }
