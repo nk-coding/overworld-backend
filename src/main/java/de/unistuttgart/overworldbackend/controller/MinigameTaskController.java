e11c6a8b4e018b392adf02bf536e0bd26a085dcc
Max Kästner <st172096@stud.uni-stuttgart.de>
2022-10-13 21:34:24 +0200

    add private

--- src/main/java/de/unistuttgart/overworldbackend/controller/MinigameTaskController.java
+++ src/main/java/de/unistuttgart/overworldbackend/controller/MinigameTaskController.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.*;
 public class MinigameTaskController {
 
     @Autowired
-    JWTValidatorService jwtValidatorService;
+    private JWTValidatorService jwtValidatorService;
 
     @Autowired
     private MinigameTaskService minigameTaskService;
