0a60135874ba578e3280b77cbc5a41d3cef84cd7
Max Kästner <st172096@stud.uni-stuttgart.de>
2023-04-10 17:04:18 +0200

    add transactional

--- src/main/java/de/unistuttgart/overworldbackend/service/MinigameTaskStatisticService.java
+++ src/main/java/de/unistuttgart/overworldbackend/service/MinigameTaskStatisticService.java
@@ -11,8 +11,10 @@ import java.util.stream.Collectors;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional
 @Slf4j
 public class MinigameTaskStatisticService {
 
