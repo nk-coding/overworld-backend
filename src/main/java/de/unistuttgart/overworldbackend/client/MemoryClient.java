4809271c19c332d268422bea4aef301445b3dff4
Owndbyfreak <st172881@stud.uni-stuttgart.de>
2023-04-13 18:26:15 +0200

    Finished integration of minigame memory for overworld backend

---
+++ src/main/java/de/unistuttgart/overworldbackend/client/MemoryClient.java
@@ -0,0 +1,13 @@
+package de.unistuttgart.overworldbackend.client;
+
+import java.util.UUID;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@FeignClient(value = "memoryClient", url = "${memory.url}/configurations")
+public interface MemoryClient {
+    @PostMapping("/{id}/clone")
+    UUID postClone(@CookieValue("access_token") final String accessToken, @PathVariable UUID id);
+}
