62987744bc6cc327460e11cc69487547ab0f14f7
Gilian Rehm <st172032@stud.uni-stuttgart.de>
2023-04-16 12:45:12 +0200

    fix Use assertEquals instead.

--- src/test/java/de/unistuttgart/overworldbackend/KeybindingTest.java
+++ src/test/java/de/unistuttgart/overworldbackend/KeybindingTest.java
@@ -1,13 +1,5 @@
 package de.unistuttgart.overworldbackend;
 
-import static org.junit.jupiter.api.Assertions.assertSame;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.Mockito.doNothing;
-import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
 import com.fasterxml.jackson.databind.ObjectMapper;
 import de.unistuttgart.gamifyit.authentificationvalidator.JWTValidatorService;
 import de.unistuttgart.overworldbackend.data.*;
@@ -17,8 +9,6 @@ import de.unistuttgart.overworldbackend.data.mapper.PlayerMapper;
 import de.unistuttgart.overworldbackend.repositories.KeybindingRepository;
 import de.unistuttgart.overworldbackend.repositories.PlayerRepository;
 import de.unistuttgart.overworldbackend.service.PlayerService;
-import java.util.List;
-import javax.servlet.http.Cookie;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -35,6 +25,16 @@ import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
+import javax.servlet.http.Cookie;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 @AutoConfigureMockMvc
 @Transactional
 @SpringBootTest
@@ -43,9 +43,9 @@ public class KeybindingTest {
 
     @Container
     public static PostgreSQLContainer postgresDB = new PostgreSQLContainer("postgres:14-alpine")
-        .withDatabaseName("postgres")
-        .withUsername("postgres")
-        .withPassword("postgres");
+            .withDatabaseName("postgres")
+            .withUsername("postgres")
+            .withPassword("postgres");
 
     @DynamicPropertySource
     public static void properties(DynamicPropertyRegistry registry) {
@@ -99,24 +99,24 @@ public class KeybindingTest {
     @Test
     void getPlayerKeybindings_DoesNotExist_ThrowsNotFound() throws Exception {
         mvc
-            .perform(
-                get("/players/" + Integer.MAX_VALUE + "/keybindings")
-                    .cookie(cookie)
-                    .contentType(MediaType.APPLICATION_JSON)
-            )
-            .andExpect(status().isNotFound())
-            .andReturn();
+                .perform(
+                        get("/players/" + Integer.MAX_VALUE + "/keybindings")
+                                .cookie(cookie)
+                                .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andExpect(status().isNotFound())
+                .andReturn();
     }
 
     @Test
     void getPlayerKeybindings() throws Exception {
         final MvcResult result = mvc
-            .perform(get(fullURL + "/keybindings").cookie(cookie).contentType(MediaType.APPLICATION_JSON))
-            .andExpect(status().isOk())
-            .andReturn();
+                .perform(get(fullURL + "/keybindings").cookie(cookie).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andReturn();
 
         final List<KeybindingDTO> keybindingStatistics = List.of(
-            objectMapper.readValue(result.getResponse().getContentAsString(), KeybindingDTO[].class)
+                objectMapper.readValue(result.getResponse().getContentAsString(), KeybindingDTO[].class)
         );
 
         assertSame(Binding.values().length, keybindingStatistics.size());
@@ -129,13 +129,13 @@ public class KeybindingTest {
     void getPlayerKeybinding() throws Exception {
         final Binding binding = Binding.MOVE_UP;
         final MvcResult result = mvc
-            .perform(get(fullURL + "/keybindings/" + binding).cookie(cookie).contentType(MediaType.APPLICATION_JSON))
-            .andExpect(status().isOk())
-            .andReturn();
+                .perform(get(fullURL + "/keybindings/" + binding).cookie(cookie).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andReturn();
 
         final KeybindingDTO keybindingDTO = objectMapper.readValue(
-            result.getResponse().getContentAsString(),
-            KeybindingDTO.class
+                result.getResponse().getContentAsString(),
+                KeybindingDTO.class
         );
         assertSame(binding, keybindingDTO.getBinding());
     }
@@ -143,41 +143,41 @@ public class KeybindingTest {
     @Test
     void getPlayerKeybinding_WrongBinding_ThrowsBadRequest() throws Exception {
         mvc
-            .perform(
-                get(fullURL + "/keybindings/NotExistingBinding").cookie(cookie).contentType(MediaType.APPLICATION_JSON)
-            )
-            .andExpect(status().isBadRequest())
-            .andReturn();
+                .perform(
+                        get(fullURL + "/keybindings/NotExistingBinding").cookie(cookie).contentType(MediaType.APPLICATION_JSON)
+                )
+                .andExpect(status().isBadRequest())
+                .andReturn();
     }
 
     @Test
     void updatePlayerKeybinding() throws Exception {
         final Binding binding = Binding.MOVE_UP;
         final Keybinding keybinding = initialPlayer
-            .getKeybindings()
-            .stream()
-            .filter(statistic -> statistic.getBinding().equals(binding))
-            .findFirst()
-            .get();
+                .getKeybindings()
+                .stream()
+                .filter(statistic -> statistic.getBinding().equals(binding))
+                .findFirst()
+                .get();
         final KeybindingDTO keybindingDTO = keybindingMapper.keybindingStatisticToKeybindingDTO(keybinding);
         keybindingDTO.setKey("H");
         final String bodyValue = objectMapper.writeValueAsString(keybindingDTO);
         final MvcResult result = mvc
-            .perform(
-                put(fullURL + "/keybindings/" + binding)
-                    .cookie(cookie)
-                    .content(bodyValue)
-                    .contentType(MediaType.APPLICATION_JSON)
-            )
-            .andExpect(status().isOk())
-            .andReturn();
+                .perform(
+                        put(fullURL + "/keybindings/" + binding)
+                                .cookie(cookie)
+                                .content(bodyValue)
+                                .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andExpect(status().isOk())
+                .andReturn();
 
         final KeybindingDTO updatedkeybindingDTO = objectMapper.readValue(
-            result.getResponse().getContentAsString(),
-            KeybindingDTO.class
+                result.getResponse().getContentAsString(),
+                KeybindingDTO.class
         );
         assertSame(binding, updatedkeybindingDTO.getBinding());
-        assertTrue(keybindingDTO.getKey().equals(updatedkeybindingDTO.getKey()));
+        assertEquals(keybindingDTO.getKey(), updatedkeybindingDTO.getKey());
     }
 
     @Test
@@ -185,14 +185,14 @@ public class KeybindingTest {
         final KeybindingDTO keybindingDTO = new KeybindingDTO();
         final String bodyValue = objectMapper.writeValueAsString(keybindingDTO);
         mvc
-            .perform(
-                put(fullURL + "/keybindings/notExistingBinding")
-                    .cookie(cookie)
-                    .content(bodyValue)
-                    .contentType(MediaType.APPLICATION_JSON)
-            )
-            .andExpect(status().isBadRequest())
-            .andReturn();
+                .perform(
+                        put(fullURL + "/keybindings/notExistingBinding")
+                                .cookie(cookie)
+                                .content(bodyValue)
+                                .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andExpect(status().isBadRequest())
+                .andReturn();
     }
 
     @Test
@@ -200,22 +200,22 @@ public class KeybindingTest {
         final Binding binding = Binding.MOVE_UP;
         final Binding differentBinding = Binding.MOVE_DOWN;
         final Keybinding keybinding = initialPlayer
-            .getKeybindings()
-            .stream()
-            .filter(statistic -> statistic.getBinding().equals(binding))
-            .findFirst()
-            .get();
+                .getKeybindings()
+                .stream()
+                .filter(statistic -> statistic.getBinding().equals(binding))
+                .findFirst()
+                .get();
         final KeybindingDTO keybindingDTO = keybindingMapper.keybindingStatisticToKeybindingDTO(keybinding);
         keybindingDTO.setKey("H");
         final String bodyValue = objectMapper.writeValueAsString(keybindingDTO);
         final MvcResult result = mvc
-            .perform(
-                put(fullURL + "/keybindings/" + differentBinding)
-                    .cookie(cookie)
-                    .content(bodyValue)
-                    .contentType(MediaType.APPLICATION_JSON)
-            )
-            .andExpect(status().isBadRequest())
-            .andReturn();
+                .perform(
+                        put(fullURL + "/keybindings/" + differentBinding)
+                                .cookie(cookie)
+                                .content(bodyValue)
+                                .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andExpect(status().isBadRequest())
+                .andReturn();
     }
 }
